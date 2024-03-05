e6c4ee761aa9868fae22ecda798ed1b5916c7077
Tan <tanishamahmud13@gmail.com>
2024-03-05 11:48:45 -0500

    server connection to database

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4b7c630e9b5bdffd"; }
+/******/ 		__webpack_require__.h = function() { return "f979b5a5ef8ce7e7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- server/server.js
+++ server/server.js
@@ -1,14 +1,40 @@
 const express = require("express");
 const cors = require('cors')
-const app = express();
+const Sequelize = require('sequelize');
+const config = require('./config');
 
+const app = express();
 app.use(cors());
+
+const port = process.env.PORT || 8080;
+
+const dbConfig = config.development; 
+
+//const sequelize = new Sequelize('database-1.cxey2w8s8sk8.us-east-2.rds.amazonaws.com')
+
+ const sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    {
+        host: dbConfig.host,
+        port: dbConfig.port,
+        dialect: dbConfig.dialect,
+    }
+);
 
 app.get("/api/home", (req, res) => {
     res.json({message: "Hello World!"});
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+      console.log('Connection to the database has been established successfully.');
+    });
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  }); 
