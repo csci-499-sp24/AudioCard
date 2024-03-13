a9056640a7055a61fb663e45db150799d4798743
Kevin <kevinfong21@gmail.com>
2024-03-13 16:16:32 -0400

    modified cardset route to count card amount in each cardset + styling

--- client/pages/explore.js
+++ client/pages/explore.js
@@ -18,28 +18,36 @@ const Explore = () => {
               for (let cardset of cardsetsData) {
                 cardset.title = cardset.title.charAt(0).toUpperCase() + cardset.title.slice(1);
               }
-              setCardsets(cardsetsWithUsernames);
+              setCardsets(cardsetsData);
         } catch (error) {
             console.error("Error fetching card sets:", error);
         }
     };
 
     return (
-        <div className="container mt-5">
-            <h1>Explore Cardsets</h1>
+        <div className="container mt-4">
+            <h1 className="mb-4">Explore Cardsets</h1>
             <div className="row">
                 {cardsets.map((cardset) => (
-                    <div key={cardset.id} className="col-sm-6 col-md-4 col-lg-3 mb-4    ">
+                    <div key={cardset.id} className="col-sm-6 col-md-4 col-lg-3 mb-4">
                         <div className="card h-100">
                             <div className="card-body">
                                 <h5 className="card-title">{cardset.title}</h5>
-                                <p>Subject: {cardset.subject}</p>
-                                <p className="card-text"><small className="text-muted">Created at: {new Date(cardset.createdAt).toLocaleDateString()}</small></p>
+                                <p className="card-subject">Subject: {cardset.subject}</p>
+                                <p className="card-count">{cardset.flashcardCount} flashcard</p>
+                                <p className="card-createdTime"><small className="text-muted">Created at: {new Date(cardset.createdAt).toLocaleDateString()}</small></p>
                             </div>
                         </div>
                     </div>
                 ))}
             </div>
+            <style jsx>{`
+                .card:hover {
+                    transform: scale(1.03); 
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); 
+                    transition: transform 0.3s ease, box-shadow 0.3s ease; 
+                }
+            `}</style>
         </div>
     );
 }
