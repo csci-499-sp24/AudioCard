986ad01101d78c413ae948885e0b3bd8fdfa6038
Tan <tanishamahmud13@gmail.com>
2024-03-21 17:00:47 -0400

    reworking stt

--- server/routes/speechIncoming.js
+++ server/routes/speechIncoming.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { SpeechClient } = require('@google-cloud/speech');
+const fs = require('fs');
 
 
 const client = new SpeechClient();
@@ -13,25 +14,25 @@ const config = {
 
 router.route('/')
 .post(async (req, res) => {
-    try {
-      const audioData = req.body.audioData;
-      const request = {
+  try {
+    const audioData = req.body.audio;
+    const request = {
         audio: {
-          content: audioData,
+            content: audioData
         },
         config: config,
-      };
-      const [response] = await client.recognize(request);
-  
-      const transcription = response.results
+    };
+    const [response] = await client.recognize(request);
+
+    const transcription = response.results
         .map(result => result.alternatives[0].transcript)
         .join('\n');
-  
-        res.status(200).json({ transcription });
-    } catch (error) {
-      console.error('Error during transcription:', error);
-      res.status(500).send('Error occurred during transcription.');
-    }
-  });
+
+    res.status(200).json({ transcription });
+} catch (error) {
+    console.error('Error during transcription:', error);
+    res.status(500).send('Error occurred during transcription.');
+}
+});
 
   module.exports = router;
