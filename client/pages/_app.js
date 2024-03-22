07efa025b9765fb1e7a7cdead8dfbbbee74c362d
Tan <tanishamahmud13@gmail.com>
2024-03-22 12:09:31 -0400

    Trying to resolve bootstrap render issues

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,5 @@
-import Head from "next/head"; 
-import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
 import "/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
