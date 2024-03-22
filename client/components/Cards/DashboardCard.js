8dbb7c2b74a71302abc347c39d94899e33d40195
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2024-03-22 17:31:53 -0400

    Added dropdown list of subjects to create a cardset form

--- client/components/Cards/DashboardCard.js
+++ client/components/Cards/DashboardCard.js
@@ -3,17 +3,20 @@ import styles from '../../styles/dashboardCard.module.css';
 
 export const DashboardCard = ({ cardset, onClick }) => {
     console.log('cardset ', cardset);
+
     return (
         <div key={cardset.id} class="col" onClick={onClick}>
             <div class="card h-100">
                 <div className="card-body">
                     <h2 className="card-title">{cardset.title}</h2>
                     <div className="mt-3">
-                        <span className={styles.cardSubject}>{cardset.subject}</span>
-                        </div>
+                        <span className={styles.cardSubject}>
+                            {cardset.subject}
+                        </span>
+                    </div>
                     <div className="my-4">
                         <span className="card-count">{cardset.flashcardCount} terms</span>
-                        </div>
+                    </div>
                 </div>
                 <div className="card-footer d-flex justify-content-end p-3" id={styles.cardFooter}>
                     <a href="#" class="btn btn-outline-dark">Edit</a>
