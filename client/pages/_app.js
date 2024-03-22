e775f7edb1191dc4f6e9671d8cd1fbbbf49d04b3
Tan <tanishamahmud13@gmail.com>
2024-03-22 11:39:23 -0400

    Trying to fix bootstrap deployment

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
+import Head from "next/head"; 
 import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
 import "/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  
-  useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
-
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
