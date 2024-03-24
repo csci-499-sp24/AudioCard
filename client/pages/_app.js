43d2d662706ce63cce503c028eb6115a7ac08368
Tan <tanishamahmud13@gmail.com>
2024-03-24 01:32:07 -0400

    FIXED BOOTSTRAP IMPORT

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,20 @@
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import "/styles/globals.css";
 import { DarkModeProvider } from '../utils/darkModeContext';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '/bootstrap/dist/js/bootstrap.bundle.min.js';
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [router.pathname]); 
   return (
     <DarkModeProvider>
       <Component {...pageProps} />
