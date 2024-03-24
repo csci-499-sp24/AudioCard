dc246975c5c900505bad3cd03b35845459a8545e
Tan <tanishamahmud13@gmail.com>
2024-03-24 01:57:45 -0400

    FIX BOOTSTRAP IMPORT PART 4

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -13,7 +13,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script
+          async
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        ></script>
       </body>
     </Html>
   );
