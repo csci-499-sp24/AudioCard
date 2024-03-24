9aaa8361fd63c3f86a52110a5601e952375105bf
Kevry Paulino <kevryp@gmail.com>
2024-03-24 11:14:30 -0400

    Search Functionality with limiters based on subject and fields

--- client/components/Explore/SearchBar.js
+++ client/components/Explore/SearchBar.js
@@ -4,27 +4,48 @@ export const SearchBar = ({ cardsets, onSearchUpdate }) => {
     const [searchInput, setSearchInput] = useState('');
     const [sortingBy, setSortingBy] = useState('');
     const [filteredCardsets, setFilteredCardsets] = useState([]);
+    const [subject, setSubject] = useState('');
+    const [searchBy, setSearchBy] = useState('title');
 
     useEffect(() => {
         onSearchUpdate(filteredCardsets);
-    },[filteredCardsets])
+    },[filteredCardsets]);
 
     useEffect(() => {
-        filterCardsets();
-    }, [searchInput]);
+        filterCardsetsBySubject();
+    }, [searchInput, subject, searchBy]);
 
     const onInputChange = (e) =>{
         e.preventDefault();
         setSearchInput(e.target.value);
     }
 
-    const filterCardsets = () => {
+    const filterCardsetsBySubject = () => {
         const searchLower = searchInput.toLowerCase();
-        setFilteredCardsets([...cardsets.filter(cardset => 
-            cardset.title.toLowerCase().includes(searchLower) || 
-            cardset.subject.toLowerCase().includes(searchLower) ||
-            (cardset.user?.username && cardset.user.username.toLowerCase().includes(searchLower))
-        )]);
+        var subjectFilter = cardsets;
+        if (subject != ''){
+            subjectFilter = cardsets.filter(cardset => 
+                cardset.subject.toLowerCase().includes(subject.toLowerCase()));
+        }
+        switch(searchBy){
+            case 'title':
+                setFilteredCardsets([...subjectFilter.filter(cardset => 
+                    cardset.title.toLowerCase().includes(searchLower)
+                )]);
+                break;
+            case 'creator':
+                setFilteredCardsets([...subjectFilter.filter(cardset => 
+                    cardset.user?.username.toLowerCase().includes(searchLower)
+                )]);
+                break;
+            case 'date':
+                setFilteredCardsets([...subjectFilter.filter(cardset => 
+                    new Date(cardset.createdAt).toLocaleDateString().includes(searchLower)
+                )]);
+                break;
+            default:
+                break;
+        }
     };
 
     const onSortChange = (e, sortBy) =>{
@@ -49,15 +70,34 @@ export const SearchBar = ({ cardsets, onSearchUpdate }) => {
                 break;   
         }
         setCardsets([...sortedCardsets]);
-        filterCardsets();
+        filterCardsetsBySubject();
+    }
+
+    const onSearchTypeChange = (e, sortBy) =>{
+        e.preventDefault();
+        let sortedCardsets;
+        switch(sortBy){
+            case 'title':
+                break;
+            case 'creationOldest':
+                break;
+            case 'alphabeticalOrder':
+                break;
+            default: 
+                break;   
+        }
+        setCardsets([...sortedCardsets]);
+        filterCardsetsBySubject();
     }
 
     const sortByFlashcards = () => {
+        
         setSortingBy('Flashcard Count');
         return cardsets.sort((a,b) => b.flashcardCount - a.flashcardCount);
     }
 
     const sortByCreation = () => {
+        
         setSortingBy('Creation Date');
         return cardsets.sort((a,b) => a.createdAt.localeCompare(b.createdAt));
     }
@@ -67,11 +107,46 @@ export const SearchBar = ({ cardsets, onSearchUpdate }) => {
         return cardsets.sort((a,b) => a.title.localeCompare(b.title));
     }
 
+    const limitSearch = (subjectFilter) => {
+        const searchLower = searchInput.toLowerCase();
+        switch(searchBy){
+            case 'title':
+                setFilteredCardsets([... cardsets.filter(cardset => 
+                    cardset.title.toLowerCase().includes(searchLower)
+                )]);
+                break;
+            case 'creator':
+                setFilteredCardsets([...cardsets.filter(cardset => 
+                    cardset.user?.username.toLowerCase().includes(searchLower)
+                )]);
+                break;
+            case 'date':
+                break;
+            default:
+                break;
+        }
+    }
+
   return (
-    <div className="col-sm-6 col-md-4 col-lg-3 mb-4">
-        <div className='d-flex mb-5'>
-            <form className="form-inline" onSubmit={((e) => e.preventDefault())}>
+    <div className="col-sm-6 col-md-4 col-lg-10 mb-4">
+        <div className='d-flex mb-4'>
+            <form className="d-flex form-inline" onSubmit={((e) => e.preventDefault())}>
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onInput={(e) => onInputChange(e)}/>
+                <select class="form-select" id="subject" onChange={(e) => setSubject(e.target.value)}>
+                    <option selected hidden>Subject</option>
+                    <option value="">Any Subject</option>
+                    <option value="History">History</option>
+                    <option value="Math">Math</option>
+                    <option value="Science">Science</option>
+                    <option value="English">English</option>
+                    <option value="Programming">Programming</option>
+                    <option value="Fine Arts">Fine Arts</option>
+                    <option value="Foreign Languages">Foreign Languages</option>
+                    <option value="Nature">Nature</option>
+                    <option value="Humanities">Humanities</option>
+                    <option value="Health">Health</option>
+                    <option value="Other">Other</option>
+                </select>
             </form>
             <div className="dropdown">
                 <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -85,6 +160,20 @@ export const SearchBar = ({ cardsets, onSearchUpdate }) => {
                 </ul>
             </div>
         </div>
+        <div className='d-flex mb-5'>
+            Search By:
+            {searchBy ===  'title' ? <button type="button" class="btn btn-primary" onClick={() => setSearchBy('title')}>Title</button>
+            : <button type="button" class="btn btn-secondary" onClick={() => setSearchBy('title')}>Title</button>}
+            {searchBy ===  'creator' ? <button type="button" class="btn btn-primary" onClick={() => setSearchBy('title')}>Creator</button>
+            : <button type="button" class="btn btn-secondary" onClick={() => setSearchBy('creator')}>Creator</button>}
+            {searchBy ===  'date' ? <button type="button" class="btn btn-primary" onClick={() => setSearchBy('title')}>Date</button>
+            : <button type="button" class="btn btn-secondary" onClick={() => setSearchBy('date')}>Date</button>}
+
+
+
+
+        </div>
+
     </div>
     )
 }
