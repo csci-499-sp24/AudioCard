b67b2312863e8b0c5dea40ddd0125f28e7e59483
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2024-03-27 16:34:48 -0400

    Added a functionality for forgot password

--- client/pages/forgot-password.js
+++ client/pages/forgot-password.js
@@ -8,14 +8,64 @@ import Image from "next/image";
 import logo from '../assets/images/logo.png';
 
 const ForgotPassword = () => {
-    
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState(null);
+    const router = useRouter();
+
+    const handleReset = async (e) => {
+        e.preventDefault();
+        let userErrorMessage;
+
+        try {
+            if (email) {              
+                await sendPasswordResetEmail(auth, email);
+                console.log('Email was sent');
+            }
+        } catch (error) {
+            userErrorMessage = "Couldn't send reset email"; 
+            setError(userErrorMessage);
+        }
+    }
+
     return (
         <div className="container">
-            forgot password
+            <div className="row mt-5 justify-content-md-center">
+                <div className="col col-md-4 col-sm-12">
+                    <div className="d-flex justify-content-center">
+                        <Image src={logo} alt="logo"/>
+                    </div>
+
+                    <h2 className="m-4 text-center">Forgot Password</h2>
+
+                    { error && 
+                        <p className='text-center text-danger'>{error}</p>
+                    }
+                   
+                    <form onSubmit={handleReset}>
+                        <div className="form-group mb-3">
+                            <label className="mb-1" htmlFor="exampleInputEmail1">Email address</label>
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                value={email}
+                                className="form-control"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />                        
+                        </div>
+                        
+                        <div className='text-center mt-4'>
+                            <button type="submit" className="btn btn-secondary">Send Reset Link</button>
+                        </div>
+                    </form>
+                    
+                    <div className='text-center mt-4'>
+                        <Link href="/login" className="link-dark link-offset-2">Back to Login</Link> 
+                    </div>
+                </div>
+            </div>
         </div>
     )
     
 }
-
 
 export default ForgotPassword;
