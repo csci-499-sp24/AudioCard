fa3847ec33b47d2c6d89db9ab6786976a8bc25ec
Tan <tanishamahmud13@gmail.com>
2024-03-30 15:54:10 -0400

    Added css to clock

--- client/components/Test Mode/timerComponent.js
+++ client/components/Test Mode/timerComponent.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-
+import { useDarkMode } from '@/utils/darkModeContext';
 export const TimerComponent = ({ timeLimit, showTestResult, isFlipped, handleSubmitAnswer, isSpeakMode }) => {
     const [timeLeft, setTimeLeft] = useState(timeLimit);
+    const {isDarkMode} = useDarkMode();
 
     useEffect(() => {
         let countDown;
@@ -12,7 +13,6 @@ export const TimerComponent = ({ timeLimit, showTestResult, isFlipped, handleSub
             countDown = setInterval(() => {
                 setTimeLeft(prevTimeLeft => {
                     const updatedTimeLeft = prevTimeLeft - 1;
-                    console.log('Countdown: ', updatedTimeLeft);
                     return updatedTimeLeft;
                 });
             }, 1000);
@@ -31,13 +31,28 @@ export const TimerComponent = ({ timeLimit, showTestResult, isFlipped, handleSub
             clearInterval(countDown);
             clearTimeout(timer);
         }
-    }, [isFlipped, timeLimit, showTestResult, handleSubmitAnswer]);
+    }, [isFlipped, timeLimit, showTestResult]);
 
     return (
         <div className='clockContainer d-flex justify-content-center'>
-            <div className='row'>
+            <div className='timerCircle'>
                 <h5>{timeLeft}</h5>
             </div>
+            <style jsx>{`
+            .timerCircle {
+                width: 75px; 
+                height: 75px;
+                border-radius: 50%; 
+                background-color: ${isDarkMode ? '#2e3956' : '#e2e2e2'}; 
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-bottom: 10px;
+                border: 2px solid ${(timeLeft==0)? 'red': '#FF00FF'}; 
+              }
+              .timerCircle h5 {
+                margin: 0;
+            }`}</style>
         </div>
     );
 };
