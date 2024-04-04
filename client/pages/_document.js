363f400c3c7001e32d3277cb459e30a51a9e33cf
Tan <tanishamahmud13@gmail.com>
2024-04-03 22:05:00 -0400

    Fixing render issues

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -18,7 +18,7 @@ export default function Document() {
           async
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         ></script>
-        <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs"></script>
+        <script async src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs"></script>
       </body>
     </Html>
   );
