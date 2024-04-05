c5e6ad5412dc783bce910d7a08fb7e7ea3d02786
Kevin <kevinfong21@gmail.com>
2024-04-05 18:49:11 -0400

    show friends user avatar in friend list

--- client/components/FriendList.js
+++ client/components/FriendList.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import axios from 'axios';
 import styles from '@/styles/profile.module.css';
 import { useDarkMode } from '@/utils/darkModeContext';
+import defaultAvatar from '@/public/userAvatar.jpg';
 
 const FriendList = ({userId}) => {
     const [friends, setFriends] = useState([]);
@@ -10,22 +11,37 @@ const FriendList = ({userId}) => {
     const { isDarkMode } = useDarkMode();
 
     useEffect(() => {
-        fetchFriends();
+        if (userId) {
+            fetchFriends();
+        }
     }, [userId]);
 
     const fetchFriends = async () => {
         try {
             const response = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/${userId}/friends`);
-            setFriends(response.data);
+            const friendsWithAvatars = await Promise.all(response.data.map(async (friend) => {
+                try {
+                    const avatarResponse = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/userAvatar/avatar/${friend.username}`);
+                    friend.avatarUrl = avatarResponse.data.url;
+                } catch (error) {
+                    console.error('Error fetching avatar:', error);
+                }
+                return friend;
+            }));
+
+            setFriends(friendsWithAvatars);
         } catch (error) {
             console.error('Error fetching friends:', error);
         }
     };
-
     const navigateToUserProfile = (friendId) => {
         router.push(`/profile/${friendId}`);
     };
 
+    const setDefaultAvatar = (event) => {
+        event.target.src = '/userAvatar.jpg';
+    };
+    
     return (
         <div className={styles.friendList}>
         <h2>Friends</h2>
@@ -33,7 +49,7 @@ const FriendList = ({userId}) => {
             {friends.map((friend) => (
                 <li key={friend.id} className={isDarkMode ? styles.darkFriendListItem : styles.friendListItem } onClick={() => navigateToUserProfile(friend.id)}>
                     <div className={styles.friendAvatar}>
-                        <img src="/userAvatar.jpg" alt="User Avatar"/>
+                        <img src={friend.avatarUrl} onError={setDefaultAvatar} alt={`${friend.username}'s avatar`} />
                     </div>
                     <span className={styles.friendName} style={{}}>{friend.username}</span>
                 </li>
