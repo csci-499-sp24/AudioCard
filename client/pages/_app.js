6c6e6fe09c805560f3bb1637162601ec004ddf62
Kevin <kevinfong21@gmail.com>
2024-05-07 19:45:00 -0400

    added a check to see if user is logged in if they are in a different page redirect to welcome page

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,16 +1,31 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import '/styles/globals.css';
 import '../styles/googleLogo.css';
-import { AuthProvider } from '../utils/authcontext';
+import { AuthProvider, useAuth } from '../utils/authcontext';
 import { DarkModeProvider } from '../utils/darkModeContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <DarkModeProvider>
-        <Component {...pageProps} />
+        <AppContent Component={Component} pageProps={pageProps} />
       </DarkModeProvider>
     </AuthProvider>
   );
 }
 
-export default MyApp; 
+function AppContent({ Component, pageProps }) {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user && router.pathname !== '/login' && router.pathname !== '/signup' && router.pathname !== '/welcome') {
+      router.push('/welcome');
+    }
+  }, [user, router]);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
