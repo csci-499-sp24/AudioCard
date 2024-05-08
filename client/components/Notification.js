4140dc380c33bad90cf865f9c91bc52623b3ed30
Kevry Paulino <kevryp@gmail.com>
2024-05-07 22:30:03 -0400

    Modified notification layout and functions for handling card set reqeusts

--- client/components/Notification.js
+++ client/components/Notification.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState, useRef } from 'react';
 import styles from '../styles/notification.module.css';
 import axios from 'axios';
 import { useDarkMode } from '@/utils/darkModeContext';
+import ShareFunction from './share';
 
 const Notification = ({ userId }) => {
     const [isNotificationOpen, setIsNotificationOpen] = useState(false);
     const [notifications, setNotifications] = useState([]);
+    const [openShareModal, setOpenShareModal] = useState(false);
     const { isDarkMode } = useDarkMode();
 
     const notificationRef = useRef(null);
@@ -75,9 +77,18 @@ const Notification = ({ userId }) => {
         }
     };
 
-    const deleteCardsetNotification = async (notificationId) => {
+    const deleteCardsetNotification = async (notification) => {
         try {
-            await axios.delete(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/${userId}/notifications/cardsetNotifications/${notificationId}`);
+            await axios.delete(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/${userId}/notifications/cardsetNotifications/${notification.id}`);
+            fetchNotifications();
+        } catch (error) {
+            console.error('Error deleting notification:', error);
+        }
+    };
+
+    const acceptCardsetRequest = async (notification) => {
+        try {
+            await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/${userId}/cardsets/${notification.cardset.id}/shared/${notification.cardset.id}/share?userid=${notification.sender.id}&authority=${notification.authority}`);
             fetchNotifications();
         } catch (error) {
             console.error('Error deleting notification:', error);
@@ -89,19 +100,46 @@ const Notification = ({ userId }) => {
             <li key={notification.id} className={styles.notificationItem}>
                 {
                 notification.type === 'revoke' ? 
-                <span className={styles.notificationText}>{`Your access to card set '${notification.cardset.title}' has been revoked`}</span>
-                : notification.type === 'requestDenied' ? 
-                <span className={styles.notificationText}>{`Your request for access to card set '${notification.cardset.title}' has been denied`}</span>
-                : notification.type === 'request' ? 
-                <span className={styles.notificationText}>{`User ${notification.requestor} has requested ${notification.authority} access to card set '${notification.cardset.title}'`}</span>
-                : notification.type === 'grant' &&
-                <span className={styles.notificationText}>{`You have been granted ${notification.authority} access to card set '${notification.cardset.title}'`}</span>
-                }
-                <div className={styles.buttonGroup}>
-                <button onClick={() => deleteCardsetNotification(notification.id)} className={styles.acceptButton}>
-                        <i className="bi bi-check"></i>
-                    </button>
+                <div className={styles.notificationContainer}>
+                    <span className={styles.notificationText}>{`Your access to card set '${notification.cardset.title}' has been revoked`}</span>
+                    <div className={styles.buttonGroup}>
+                        <button onClick={() => deleteCardsetNotification(notification)} className={styles.acceptButton}>
+                            <i className="bi bi-check"></i>
+                        </button>
+                    </div>
                 </div>
+                : notification.type === 'requestDenied' ? 
+                <div className={styles.notificationContainer}>
+                    <span className={styles.notificationText}>{`Your request for access to card set '${notification.cardset.title}' has been denied`}</span>
+                    <div className={styles.buttonGroup}>
+                        <button onClick={() => deleteCardsetNotification(notification)} className={styles.acceptButton}>
+                            <i className="bi bi-check"></i>
+                        </button>
+                    </div>
+                </div>
+                : notification.type === 'request' ? 
+                <div className={styles.notificationContainer}>
+                    <span className={styles.notificationText}>{`User ${notification.sender.username} has requested ${notification.authority} access to card set '${notification.cardset.title}'`}</span>
+                    <div className={styles.buttonGroup}>
+                        <button onClick={() => acceptCardsetRequest(notification)} className={styles.acceptButton}>
+                            <i className="bi bi-check"></i>
+                        </button>
+                        <button onClick={() => deleteCardsetNotification(notification)} className={styles.declineButton}>
+                            <i className="bi bi-x"></i>
+                        </button>
+                    </div>
+                </div>
+                : notification.type === 'grant' &&
+                <div className={styles.notificationContainer}>
+                    <span className={styles.notificationText}>{`You have been granted ${notification.authority} access to card set '${notification.cardset.title}'`}</span>
+                    <div className={styles.buttonGroup}>
+                        <button onClick={() => deleteCardsetNotification(notification)} className={styles.acceptButton}>
+                            <i className="bi bi-check"></i>
+                        </button>
+                    </div>
+                </div>
+
+                }
             </li>
         );
     }
@@ -111,7 +149,7 @@ const Notification = ({ userId }) => {
             <li key={notification.id} className={styles.notificationItem}>
                 {
                 notification.type === 'pending' ?     
-                <div>
+                <div className={styles.notificationContainer}>
                     <span className={styles.notificationText}>{`${notification.sender.username} wants to be your friend`}</span>
                     <div className={styles.buttonGroup}>
                         <button onClick={() => acceptFriendRequest(notification.sender.id)} className={styles.acceptButton}>
@@ -123,7 +161,7 @@ const Notification = ({ userId }) => {
                     </div>
                 </div>
                 : notification.type === 'confirmed' ?
-                <div>
+                <div className={styles.notificationContainer}>
                     <span className={styles.notificationText}>{`${notification.sender.username} has accepted your friend request`}</span>
                     <div className={styles.buttonGroup}>
                         <button onClick={() => deleteFriendNotification(notification.id)} className={styles.acceptButton}>
@@ -132,7 +170,7 @@ const Notification = ({ userId }) => {
                     </div>
                 </div>
                 : notification.type === 'denied' ?
-                <div>
+                <div className={styles.notificationContainer}>
                     <span className={styles.notificationText}>{`${notification.sender.username} has declined your friend request`}</span>
                     <div className={styles.buttonGroup}>
                         <button onClick={() => deleteFriendNotification(notification.id)} className={styles.acceptButton}>
@@ -142,7 +180,7 @@ const Notification = ({ userId }) => {
                     </div>
                 </div>
                 : notification.type === 'unfriended' &&
-                <div>
+                <div className={styles.notificationContainer}>
                     <span className={styles.notificationText}>{`${notification.sender.username} has unfriended you`}</span>
                     <div className={styles.buttonGroup}>
                         <button onClick={() => deleteFriendNotification(notification.id)} className={styles.acceptButton}>
