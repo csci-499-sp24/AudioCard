e2e8b6db26f0dc967e06717b2df6fe55cf2b818b
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2024-05-12 17:36:07 -0400

    Changed the styles of user cards on explore page to match the others

--- client/components/Cards/UserCard.js
+++ client/components/Cards/UserCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect, useState} from 'react';
 import { useDarkMode } from '@/utils/darkModeContext';
 import styles from '../../styles/navbar.module.css';
+import style from '../../styles/termCard.module.css';
 import Link from 'next/link';
 import axios from 'axios';
 
@@ -30,7 +31,7 @@ export const UserCard = ({ user}) => {
 
   return (
     <Link className="text-decoration-none col-sm-6 col-md-4 col-lg-3 mb-4" href={`/profile/${user.id}`}>
-            <div className="card h-100" style={{ backgroundColor: isDarkMode ? '#2e3956' : 'white', color: isDarkMode ? 'white' : 'black'}}>
+            <div className="card h-100" id={`${isDarkMode ? style.flashcardDark : style.flashcard}`}>
                 <div className="card-body">
                     <div className='d-flex flex-column align-items-center'>
                                         <img src={userAvatar} onError={setDefaultAvatar} alt="User Avatar" className={styles.navUserAvatar} style={{borderColor: isDarkMode ? 'white': 'black'}} />
