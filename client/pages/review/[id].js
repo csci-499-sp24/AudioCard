71ef9b490700e845f24ebe02c6f3ca25c38b476c
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2024-05-12 11:40:35 -0400

    Centered text on review and test modes info pages

--- client/pages/review/[id].js
+++ client/pages/review/[id].js
@@ -79,35 +79,30 @@ const ReviewPage = () => {
                 </div>
             <hr style={{borderColor: isDarkMode ? 'white' : 'black', borderWidth: '2px'}}/>
             {cardsetData && startReview && <CardViewReviewMode userId={userData?.id} cardset={cardsetData} isDarkMode={isDarkMode} preferredLanguage={preferredLanguage ? preferredLanguage : cardsetData.language} />}
-            {!startReview &&  <div className='container d-flex justify-content-center mt-5' style={{backgroundColor: isDarkMode? '#252526' : 'white', width: '70%', borderRadius: '10px',}}>
+            {!startReview &&  <div className='container d-flex justify-content-center mt-5 mb-5' style={{backgroundColor: isDarkMode? '#252526' : 'white', width: '70%', borderRadius: '10px',}}>
                     <div className='container2'>
-                        <div className='headingContainer mt-5' style={{paddingLeft: '10%'}}>
+                        <div className='headingContainer mt-5'>
                             <div className='row'>
-                                <h1>Review your card set</h1>
+                                <h1 className='text-center'>Review your card set</h1>
                             </div>
                         </div>
-                        <div classname='bodyContainer' style={{paddingLeft: '10%'}}>
-                            <div className='row'>
+                        <div className='d-flex flex-column justify-content-center align-content-center'>
+                            <div className='row text-center'>
                                 <div>Welcome to review mode, a hands-free studying experience.</div>
                             </div>
-                            <div className='row mt-5' style={{color: 'green'}}>
-                                <div>&#127911; Review Mode:</div>
-                            </div>
-                            <div className='row' style={{color:'green'}}>
-                                <p style={{paddingLeft: '40px'}}>The app will speak out your flashcard terms and definitions, all you have to do is listen! &#128266;</p>
-                            </div>
-                            <div className='row' style={{color: '#FFA500'}}>
-                                <div>	&#x2699; Settings:</div>
-                            </div>
-                            <div className='row' style={{color:'#FFA500'}}>
-                                <p style={{paddingLeft: '40px'}}><i className="bi bi-arrow-clockwise"></i> Loop: put your card set on an endless loop</p>
-                                <p style={{paddingLeft: '40px'}}>&#127911;: set the gender, language, and speaking rate!</p>
-                                <p style={{paddingLeft: '40px'}}><i className="bi bi-clock"></i> Delay: seconds between question and answer</p>
-                            </div>
-                            <div className='row mb-5' style={{color:'#FFA500'}}>
-                            <p style={{paddingLeft: '40px'}}>&#128483; Enable Voice Commands: say &apos;shuffle&apos;, &apos;restart&apos;, or &apos;exit&apos;</p>
+
+                            <div className='d-flex flex-column justify-content-center mt-5'>
+                                    <div id={styles.reviewModeGreen}>&#127911; Review Mode:</div>
+                                    <p id={styles.reviewModeGreenSub}>The app will speak out your flashcard terms and definitions, all you have to do is listen! &#128266;</p>
+                                    
+                                    <div id={styles.reviewModeOrange}>	&#x2699; Settings:</div>
+                                    <p id={styles.reviewModeOrangeSub}><i className="bi bi-arrow-clockwise"></i> Loop: put your card set on an endless loop</p>
+                                    <p id={styles.reviewModeOrangeSub}>&#127911;: set the gender, language, and speaking rate!</p>
+                                    <p id={styles.reviewModeOrangeSub}><i className="bi bi-clock"></i> Delay: seconds between question and answer</p>
+                                    <p className='mb-5' id={styles.reviewModeOrangeSub}>&#128483; Enable Voice Commands: say &apos;shuffle&apos;, &apos;restart&apos;, or &apos;exit&apos;</p>
                             </div>
                         </div>
+                        
                         <div className='row mb-5'>
                             <div className='col d-flex justify-content-center'>
                             <button className={styles.startButton} onClick={() => setStartReview(true)}>Start</button>
