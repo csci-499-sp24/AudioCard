eae5f08639dd0abf9daea92635a2c76c7385e347
Kevin <kevinfong21@gmail.com>
2024-05-12 21:57:39 -0400

    added local storage to hold user auth

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -17,19 +17,24 @@ function MyApp({ Component, pageProps }) {
 
 
 function AppContent({ Component, pageProps }) {
-  const { user } = useAuth();
+  const { user, setUser } = useAuth();
   const router = useRouter();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-      const timer = setTimeout(() => {
-        if (!user && !router.pathname.startsWith('/cardsets/') && router.pathname !== '/login' && router.pathname !== '/signup' && router.pathname !== '/welcome') {
-          router.push('/welcome');
-        }
-        setLoading(false);
-      }, 100); 
+    const storedUser = JSON.parse(localStorage.getItem('user'));
+    if (storedUser && JSON.stringify(storedUser) !== JSON.stringify(user)) {
+      setUser(storedUser);
+    }
 
-      return () => clearTimeout(timer);
+    const timer = setTimeout(() => {
+      if (!user && !router.pathname.startsWith('/cardsets/') && router.pathname !== '/login' && router.pathname !== '/signup' && router.pathname !== '/welcome') {
+        router.push('/welcome');
+      }
+      setLoading(false);
+    }, 100); 
+
+    return () => clearTimeout(timer);
   }, [user, router]);
 
   if (loading) {
--- client/utils/authcontext.js
+++ client/utils/authcontext.js
@@ -7,19 +7,24 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
     useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+
         const unsubscribe = auth.onAuthStateChanged((authUser) => {
             setUser(authUser);
+            localStorage.setItem('user', JSON.stringify(authUser));
         });
 
         return () => unsubscribe();
     }, []);
 
     return (
-        <AuthContext.Provider value={{ user }}>
+        <AuthContext.Provider value={{ user, setUser }}>
             {children}
         </AuthContext.Provider>
     );
 };
 
 export const useAuth = () => useContext(AuthContext);
-
